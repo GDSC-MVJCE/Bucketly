84955f43f27c9dfb8d026876a02e46a8acf5b698
Vector-ops <sumith2827@gmail.com>
2024-05-05 10:28:22 +0530

    add routes

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const connectDB = require("./db/connect");
+const router = require("./routes/router");
 
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+// routes
+app.use("/api/v1/items", router);
+
+// start server
 const start = async () => {
 	try {
+		await connectDB(process.env.MONGO_URI);
 		app.listen(process.env.PORT, () => {
 			console.log(`Server is running on port ${process.env.PORT}`);
 		});
