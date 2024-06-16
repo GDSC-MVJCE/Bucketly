d5e901a0592993913deee0a30fda2a990a55a5fa
Shivam77607 <shivamsharma77607@gmail.com>
2024-06-16 09:58:33 +0530

    fix: error.js import

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,12 +1,12 @@
-const { CustomAPIError } = require("../errors/error");
+const { CustomAPIError } = require("../utils/error");
 
 const errorHandler = (err, req, res, next) => {
-	if (err instanceof CustomAPIError) {
-		return res.status(err.statusCode).json({ message: err.message });
-	}
-	return res
-		.status(500)
-		.json({ message: "Something went wrong, please try again" });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res
+    .status(500)
+    .json({ message: "Something went wrong, please try again" });
 };
 
 module.exports = errorHandler;
--- server/middleware/notFound.js
+++ server/middleware/notFound.js
@@ -1,7 +1,7 @@
-const { createCustomError } = require("../errors/error");
+const { createCustomError } = require("../utils/error");
 
 const notFound = (req, res, next) => {
-	next(createCustomError("Page not found", 404));
+  next(createCustomError("Page not found", 404));
 };
 
 module.exports = notFound;
